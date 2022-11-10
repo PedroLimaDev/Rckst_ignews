8f9e74c71de9fdbc570f6ccd571ff32100477fef
Pedro Fellipe <49622290+PedroLimaDev@users.noreply.github.com>
2022-11-10 20:42:21 -0300

    Add Next-Auth and Github SignIn

--- src/components/SignInButton/SignInButton.tsx
+++ src/components/SignInButton/SignInButton.tsx
@@ -1,24 +1,30 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 import styles from "./SignInButton.module.scss";
 
 const SignInButton = () => {
-	const isUserLoggedIn = true;
+	const { data: session } = useSession();
 
-	return isUserLoggedIn ? (
+	console.log("session =>> ", session);
+
+	return session ? (
 		<button 
 			type="button"
 			className={styles.signInButton}
+			onClick={() => signOut()}
 		>
 			<FaGithub  color="#04d361" />
-      Sign in with Github
+			{session.user?.name}
 			<FiX color="#737380" className={styles.closeIcon} />
 		</button>
 	) : (
 		<button 
 			type="button"
 			className={styles.signInButton}
+			onClick={() => signIn("github")}
 		>
 			<FaGithub  color="#eba417" />
       Sign in with Github
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { isNil } from "ramda";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import SubscribeButton from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 	);
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 	const price = await stripe.prices.retrieve("price_1M20f4FUGLEhO7sBPev5Kw9q", {
 		expand: ["product"]
 	});
@@ -57,6 +57,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 	return {
 		props: {
 			product,
-		}
+		},
+		revalidate: 60 * 60 * 24,
 	};
 };
