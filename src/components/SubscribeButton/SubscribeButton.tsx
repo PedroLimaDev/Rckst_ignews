e56db51409907b566198049ac7d797dd5eae7804
Pedro Fellipe <49622290+PedroLimaDev@users.noreply.github.com>
2022-11-27 15:37:43 -0300

    Add Subscribe Action

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,14 +1,41 @@
+import { useSession, signIn } from "next-auth/react";
+
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
+
 import styles from "./SubscribeButton.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-const SubscribeButton = ({ priceId}: SubscribeButtonProps) => {
+const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+	const { data: session } = useSession();
+
+	const handleSubscribe = async () => {
+		if (!session) {
+			signIn("github");
+			return;
+		}
+
+		try {
+			const response = await api.post("/subscribe");
+
+			const { sessionId } = response.data;
+
+			const stripe = await getStripeJS();
+
+			await stripe?.redirectToCheckout({ sessionId } );
+		} catch (err: any) {
+			alert(err?.message);
+		}
+	};
+
 	return (
 		<button 
 			type="button"
 			className={styles.subscribeButton}
+			onClick={handleSubscribe}
 		>
       Subscribe Now
 		</button>
